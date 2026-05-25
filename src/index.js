e94b3e901ed2060d21cef72b3556ef68390af638
mohammed nur <mohamdkarta2@gmail.com>
2026-05-25 21:39:46 +0300

    scaling profile img after clicking

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const lines = document.querySelectorAll('.bottom_section .line');
+const sideBarImg = document.getElementById("side_bar_img");
+
+
+
+// Add Active Class To Lines
 
 
 lines.forEach((ele) => {
@@ -13,3 +18,38 @@ lines.forEach((ele) => {
 
     })
 })
+
+// Add Active Class To Lines
+
+
+
+sideBarImg.onclick = function (e) {
+  e.stopPropagation();
+  const div = document.createElement("div");
+  div.classList.add("img_background")
+  div.style.height = '50%';
+  div.style.width = '50%';
+  div.style.background = 'white'
+  div.style.position = "absolute"
+  div.style.left = '50%'
+  div.style.top = '50%'
+  div.style.transform = 'translate(-50%, -50%)'
+  div.style.borderRadius = '20px'
+  div.style.display = 'flex'
+  div.style.justifyContent = 'center';
+  div.style.alignItems = 'center'
+  div.style.width = 'fit-content'
+  div.style.padding = '20px'
+  document.body.appendChild(div)
+
+  const img = document.createElement("img")
+  img.style.width = '400px'
+  img.src = this.src
+  div.appendChild(img)
+}
+
+document.addEventListener("click", (e) => {
+    
+    document.querySelector(".img_background").remove()
+})
+
